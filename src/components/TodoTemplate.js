e1a298b99ac5b29bb679de05365f5db7103166a5
ohyoonah <dhdbsdk3@naver.com>
2022-11-05 00:40:18 +0900

    아이템 id값 변경

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 import TodoHead from "./TodoHead";
 import TodoInsert from "./TodoInsert";
@@ -48,17 +48,14 @@ const TodoTemplate = () => {
     setNewText(todo);
   };
 
-  const nextId = useRef(0);
-
   const onInsert = (text) => {
     const todo = {
-      id: nextId.current,
+      id: Date.now(),
       text,
       checked: false,
       important: false,
     };
     setTodos(todos.concat(todo));
-    nextId.current += 1;
   };
 
   const onToggle = (id) => {
